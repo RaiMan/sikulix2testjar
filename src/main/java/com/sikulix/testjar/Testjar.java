a5dd325ad8a92490527c2a538f9b5df3a9e2150d
RaiMan <rmhdevelop@me.com>
2017-04-15 09:28:07 +0200

    initial commit

---
+++ src/main/java/com/sikulix/testjar/Testjar.java
@@ -0,0 +1,7 @@
+package com.sikulix.testjar;
+
+public class Testjar {
+  public static void main(String[] args) {
+    System.out.println("com.sikulix.testjar.Testjar: nothing to do here");
+  }
+}
